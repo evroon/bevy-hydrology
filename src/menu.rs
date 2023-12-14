378a891aa15387a4e0acb578faf5c43c2a60ddeb
Erik Vroon <erik.vroon@channable.com>
2023-12-14 21:27:12 +0100

    Fix release build by dynamically linking

--- src/menu.rs
+++ src/menu.rs
@@ -10,7 +10,7 @@ impl Plugin for MenuPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<ButtonColors>()
             .add_systems(OnEnter(GameState::Menu), setup_menu)
-            // .add_systems(Render, click_play_button.in_set(Update(GameState::Menu)))
+            .add_systems(Update, click_play_button.run_if(in_state(GameState::Menu)))
             .add_systems(OnExit(GameState::Menu), cleanup_menu);
     }
 }
