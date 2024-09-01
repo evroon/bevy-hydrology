90c782c45aca4d3447f6e0a5be300bda24cfc0cf
Erik Vroon <erik.vroon@channable.com>
2024-09-01 13:47:32 +0200

    Update all dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // disable console on windows for release builds
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use bevy::asset::AssetMetaCheck;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 use bevy::winit::WinitWindows;
@@ -12,22 +13,30 @@ use winit::window::Icon;
 fn main() {
     App::new()
         .insert_resource(Msaa::Off)
-        .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Bevy hydrology".to_string(),
-                resolution: (1920., 1040.).into(),
-                canvas: Some("#bevy".to_owned()),
-                // canvas {
-                //     width: 100%;
-                //     height: 100%;
-                // },
-                prevent_default_event_handling: false,
-                ..default()
-            }),
+        .insert_resource(ClearColor(Color::linear_rgb(0.4, 0.4, 0.4)))
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Bevy hydrology".to_string(),
+                        resolution: (1920., 1040.).into(),
+                        canvas: Some("#bevy".to_owned()),
+                        // canvas {
+                        //     width: 100%;
+                        //     height: 100%;
+                        // },
+                        prevent_default_event_handling: false,
+                        fit_canvas_to_parent: true,
+                        ..default()
+                    }),
 
-            ..default()
-        }))
+                    ..default()
+                })
+                .set(AssetPlugin {
+                    meta_check: AssetMetaCheck::Never,
+                    ..default()
+                }),
+        )
         .add_systems(Startup, set_window_icon)
         .add_plugins(GamePlugin)
         .run();
